9277f012c3f17ac8b2b0c8cc9567db8407483226
mambax7 <mambax7@gmail.com>
2015-05-05 09:40:39 -0400

    Original files

---
+++ xsoft/clock.php
@@ -0,0 +1,23 @@
+<?php
+
+####################################
+##                                ##
+##      Made by Bananiel          ##
+##                                ##
+## Comments 2 scripts@bananiel.nl ##
+## 4more scripts take a look @    ##
+##   http://www.bananiel.nl       ##
+##                                ##
+####################################
+
+$seperator =":";
+
+$hour = date("G");
+$minute = date("i");
+$second = date("s");
+
+
+
+echo "<FONT color=#000000><B>$hour$seperator$minute$seperator$second</B></FONT>";
+
+?>
